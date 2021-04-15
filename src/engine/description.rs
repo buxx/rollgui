537010047613be64b1a1347cf019a8169a315217
Sevajol Bastien <contact@bux.fr>
2021-04-15 15:42:37 +0200

    do not consider choices as pure text

--- src/engine/description.rs
+++ src/engine/description.rs
@@ -913,7 +913,16 @@ impl DescriptionEngine {
 }
 
 fn part_is_pure_text(part: &Part) -> bool {
-    (part.text.is_some() || part.label.is_some()) && !part.is_link && part.type_.is_none() && !part.is_checkbox
+    let is_choice = if let Some(choices) = &part.choices {
+        choices.len() != 0
+    } else {
+        false
+    };
+    (part.text.is_some() || part.label.is_some())
+        && !part.is_link
+        && part.type_.is_none()
+        && !part.is_checkbox
+        && !is_choice
 }
 
 fn get_pure_text_class(item: &Part) -> Option<text::Class> {
